cebb80649d56f5a395e775436cca2fa5c489eff4
SarAbigail <samihimezu@gmail.com>
2019-09-24 14:46:52 -0500

    Test stats

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,9 @@
+/* eslint-disable max-len */
 // importamos la funcion que vamos a testear
 import { io } from './input-output.js';
-import { convertRelativePathToAbsolutePath, isFile, isDirectory, isMd, walkDirectory, saveLinks, stats, statsValidate } from '../src/main';
+import {
+  convertRelativePathToAbsolutePath, isFile, isDirectory, isMd, walkDirectory, saveLinks, validate, stats, statsValidate,
+} from '../src/main';
 
 const path = require('path');
 
@@ -74,6 +77,40 @@ describe('Save all links in an array of objects (href,text,file)', () => {
   it('Debería mostrar la ruta de donde procede el link', () => {
     expect(saveLinks([path.join(process.cwd(), 'test', 'prueba', '1.md')])[0].thePath).toBe(path.join(process.cwd(), 'test', 'prueba', '1.md'));
   });
+});
+
+describe('Validate link', () => {
+  it('Debería ser una función', (done) => {
+    expect(typeof validate).toBe('function');
+    done();
+  });
+
+  it('Debería devolver un array de objetos con la ruta, url, status, status text y text', done => validate(io.arrObj)
+    .then((data) => {
+      expect(data).toStrictEqual([{
+        thePath: path.join(process.cwd(), 'test', 'prueba', '1.md'),
+        href: 'https://nodejs.org/es/',
+        status: 200,
+        statusText: 'OK',
+        text: 'Node.js',
+      },
+      {
+        thePath: path.join(process.cwd(), 'test', 'prueba', '1.md'),
+        href: 'https://github.com/karenLee57/karenLee57.github.io-hangman',
+        status: 404,
+        statusText: 'FAIL',
+        text: 'Repositorio',
+      },
+      {
+        thePath: '/home/laboratoria/LIM010-fe-md-links/test/prueba/1.md',
+        status: 'ERROR',
+        statusText: 'FAIL',
+        href: 'https://www.facebooks.com/',
+        text: 'Facebook',
+      },
+      ]);
+      done();
+    }));
 });
 
 describe('Show stadistics about md-links', () => {
